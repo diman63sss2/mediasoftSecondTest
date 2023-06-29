79cb416dce83cd3742c0a5aa7cccc7d9fb8bc0fd
diman <diman63sss2@mail.ru>
2023-06-29 08:02:38 +0400

    0.3.08 Add themes, variables, ThemeContext.ts, ThemeProvider.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React, { Suspense } from "react";
-import './index.scss';
+import React, {Suspense} from "react";
+import './styles/index.scss';
 import { Link, Route, Routes } from "react-router-dom";
 import { CartPageAsync } from "./pages/CartPage/CartPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
+
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>
+        TOGGLE THEME.
+      </button>
       <Link to={'/'}>
         Главная
       </Link>
