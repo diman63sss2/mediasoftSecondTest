2f6d7f7ec020144e4401d8ce521b494f5b0a6c5f
diman <diman63sss2@mail.ru>
2023-07-11 17:45:33 +0400

    0.3.1.10 add loader and NotFoundPage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,23 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    element={element}
-                    path={path}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+    <div className="container">
+        <Suspense fallback={<PageLoader />}>
+            <Routes>
+                {Object.values(routeConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        element={element}
+                        path={path}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    </div>
+
 );
 
 export default AppRouter;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -2,17 +2,20 @@ import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { CartPage } from 'pages/CartPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRouter {
   MAIN = 'main',
   CART = 'cart',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found',
 }
 
 export const RoutePath: Record<AppRouter, string> = {
     [AppRouter.MAIN]: '/',
     [AppRouter.CART]: '/cart',
     [AppRouter.ABOUT]: '/about',
+    [AppRouter.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRouter, RouteProps> = {
@@ -27,6 +30,10 @@ export const routeConfig: Record<AppRouter, RouteProps> = {
     [AppRouter.ABOUT]: {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    [AppRouter.NOT_FOUND]: {
+        path: RoutePath.not_found,
+        element: <NotFoundPage />,
     },
 
 };
